1c2b38625349f903630a9e20d896a6d5fc134d06
DailyBuild <kanehori@haselab.net>
2019-04-24 10:34:36 +0900

    ビルドエラーを避けるための緊急措置

---
+++ src/GL/freeglut_ext.h
@@ -0,0 +1,14 @@
+﻿/*
+ *  Copyright (c) 2003-2006, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
+#ifdef _MSC_VER
+# include "win32/freeglut_ext.h"
+#elif __linux__
+# include "linux/freeglut_ext.h"
+#else
+# error "freeglut_ext is not available on this platform."
+#endif
---
+++ src/GL/freeglut_std.h
@@ -0,0 +1,14 @@
+﻿/*
+ *  Copyright (c) 2003-2006, Shoichi Hasegawa and Springhead development team 
+ *  All rights reserved.
+ *  This software is free software. You can freely use, distribute and modify this 
+ *  software. Please deal with this software under one of the following licenses: 
+ *  This license itself, Boost Software License, The MIT License, The BSD License.   
+ */
+#ifdef _MSC_VER
+# include "win32/freeglut_std.h"
+#elif __linux__
+# include "linux/freeglut_std.h"
+#else
+# error "freeglut_std is not available on this platform."
+#endif
